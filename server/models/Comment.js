16cecbdefa9a349071ad0338d262854a1b3ffbd7
Ayush Singh <ayush@quickblog.dev>
2026-06-30 18:59:31 +0530

    refactor: add trim validators to Comment schema fields

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const commentSchema = new mongoose.Schema({
-    blog: {type: mongoose.Schema.Types.ObjectId, ref: 'blog', required: true},
-    name:  { type: String, required: true },
-    content: { type: String, required: true },
+    blog: { type: mongoose.Schema.Types.ObjectId, ref: 'blog', required: true },
+    name: { type: String, required: true, trim: true },
+    content: { type: String, required: true, trim: true },
     isApproved: { type: Boolean, default: false },
-},{timestamps: true});
+}, { timestamps: true });
 
 const Comment = mongoose.model('Comment', commentSchema);
 
