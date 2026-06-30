e247c931f8379e4f55ebf4cec03977ea8b2b0295
Ayush Singh <ayush@quickblog.dev>
2026-06-30 18:52:02 +0530

    refactor: use CategoryFilter in BlogList and add empty state

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,44 +1,33 @@
 import React, { useState } from 'react'
-import { motion } from "motion/react"
-import { BLOG_CATEGORIES } from '../utils/constants'
 import BlogCard from './BlogCard'
+import CategoryFilter from './CategoryFilter'
 import { useAppContext } from '../context/AppContext'
 
 const BlogList = () => {
 
     const [menu, setMenu] = useState("All")
-    const {blogs, input} = useAppContext()
+    const { blogs, input } = useAppContext()
 
-    const filteredBlogs = ()=>{
-      if(input === ''){
-        return blogs
-      }
-      return blogs.filter((blog)=> blog.title.toLowerCase().includes(input.toLowerCase()) || blog.category.toLowerCase().includes(input.toLowerCase()))
-    }
+    const filteredBlogs = blogs
+        .filter((blog) =>
+            input === ''
+                ? true
+                : blog.title.toLowerCase().includes(input.toLowerCase()) ||
+                  blog.category.toLowerCase().includes(input.toLowerCase())
+        )
+        .filter((blog) => menu === "All" ? true : blog.category === menu)
 
-  return (
-    <div>
-      <div className='flex justify-center gap-4 sm:gap-8 my-10 relative'>
-        {BLOG_CATEGORIES.map((item)=> (
-            <div key={item} className='relative'>
-                <button onClick={()=> setMenu(item)}
-                 className={`cursor-pointer text-gray-500 ${menu === item && 'text-white px-4 pt-0.5'}`}>
-                    {item}
-                    {menu === item && (
-                        <motion.div layoutId='underline' 
-                        transition={{type: 'spring', stiffness: 500, damping: 30}}
-                        className='absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full'></motion.div>
-                    )}
-                    
-                </button>
+    return (
+        <div>
+            <CategoryFilter activeCategory={menu} onSelect={setMenu} />
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40'>
+                {filteredBlogs.length > 0
+                    ? filteredBlogs.map((blog) => <BlogCard key={blog._id} blog={blog} />)
+                    : <p className='text-gray-400 col-span-full text-center py-10'>No blogs found.</p>
+                }
             </div>
-        ))}
-      </div>
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40'>
-        {filteredBlogs().filter((blog)=> menu === "All" ? true : blog.category === menu).map((blog)=> <BlogCard key={blog._id} blog={blog}/>)}
-      </div>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default BlogList
